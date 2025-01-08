78837f0128084a7002c662248607c75829060fb9
shivamshrivaastava <shivamshrivaastava@gmail.com>
2025-01-08 20:51:23 +0530

    updated

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -6,5 +6,26 @@ todoRouter.post("/add", async (req,res)=>{
     await todoModel.insertMany([req.body])
     res.send("todo added")
 })
+todoRouter.get("/get/:id", async (req, res) => {
+    const todo = await todoModel.findById(req.params.id);
+    res.send(todo);
+});
+todoRouter.put("/update/:id", async (req, res) => {
+    await todoModel.updateOne({ _id: req.params.id }, { $set: req.body });
+    res.send("todo updated");
+});
+
+todoRouter.patch("/update/:id", async (req, res) => {
+    await todoModel.updateOne({ _id: req.params.id }, { $set: req.body });
+    res.send("todo partially updated");
+});
+
+todoRouter.delete("/delete/:id", async (req, res) => {
+    await todoModel.deleteOne({ _id: req.params.id });
+    res.send("todo deleted");
+});
+
+
+
 
 module.exports = todoRouter;
