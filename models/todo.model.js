07eefedc33409f83683d08d28dd385ca2609a963
shivamshrivaastava <shivamshrivaastava@gmail.com>
2025-01-09 21:00:07 +0530

    backend

--- models/todo.model.js
+++ models/todo.model.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
+
 const TodoSchema = new mongoose.Schema({
-    name: String,
-    status: Boolean,
+    name: { 
+        type: String, 
+        required: true
+    },
+    status: { 
+        type: Boolean, 
+        required: true
+    },
     description: String,
-    deadline:String,
+    deadline: String,
+    gender: { 
+        type: String, 
+        enum: ['male', 'female', 'other'], 
+        required: true
+    },
+    age: { 
+        type: Number, 
+        required: true,
+        min: 20,
+        max: 100
+    },
 });
 
- const  todoModel = mongoose.model("todos", TodoSchema)
+const todoModel = mongoose.model("todos", TodoSchema);
 
- module.exports = todoModel;
+module.exports = todoModel;
